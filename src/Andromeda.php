74cbfa6c20056d5a64d60f40b85645a625f6b3aa
wuding <netjoin@gmail.com>
2022-05-10 02:15:36 +0800

    附加自定义参数，变量提取，类型判断

--- src/Andromeda.php
+++ src/Andromeda.php
@@ -2,44 +2,48 @@
 
 #$result_values = require 'Andromeda.php';
 
-function andromeda($undefined_functions, $chars, $tags = 'pre', $attributes = 'true')
+function andromeda($undefined_functions, $chars, $tags = 'pre', $attributes = 'true', $options = array())
 {
+extract($options['func']['extract']['var_array'] ?? array());
+
 /*
  * 输入
  */
-if ($tags) {
+if (is_string($tags)) {
     echo '<'. $tags .' contenteditable="'. $attributes .'">';
 }
 
 /* 参数 */
+if (is_int($arg)) {
 print_r([
-'func_get_args' => func_get_args(),#[1]
-'func_get_arg' => func_get_arg(0),
+'func_get_args' => func_get_args(),
+'func_get_arg' => func_get_arg($arg),
 'func_num_args' => func_num_args(),
 ]);
+}
+
 
 
 /* 错误报告 */
+if ($err) {
 echo "<b>Fatal error</b>: " . PHP_EOL;
 echo "Uncaught Error: " . PHP_EOL;
 echo "Call to " . PHP_EOL;
+}
+
 
 
 /* 看见看不见，覆盖值 */
-$_undefinedFunction = func_get_args()[0];
-$_undefinedFunction = func_get_arg(0);
-$_undefinedFunction[31] = 'mysql_close';
+if (is_array($var)) {
 
 /**/
-var_export
-(
-[
- ' undefined function ' => $_undefinedFunction,
-]
-);
+if ( true === $options['func']['']['var_export'] ) {
+    var_export( [' undefined function ' => $var ] );
+}
+
 
 $stack_trace = array();
-foreach ($_undefinedFunction as $key => $value) {
+foreach ($var as $key => $value) {
     $var = isset($stack_trace[$value]);
     if (!$var) {
         $stack_trace[$value] = 1;
@@ -48,28 +52,58 @@ foreach ($_undefinedFunction as $key => $value) {
         }
     }
 }
+}
 
 /*
  * 链接
  */
-$func_get_arg = func_get_arg(1);
+if (is_int($lnk)) {
+$func_get_arg = func_get_arg($lnk);
 foreach ($func_get_arg as $arg) {
     print_r('http://192.168.100.4:60917/?q='. $arg . PHP_EOL);
 }
-
+}
 
 /*
  * 输出
  */
-if ($tags) {
+if (is_string($tags)) {
     echo '</'. $tags .'>';
 }
 }
 
-/**/
-andromeda([
+/*
+andromeda(
+
+[
 'mysql_connect',
 'mysql_select_db',
 'mysql_set_charset',
 'mysql_query',
-], array(), $_GET['pre'] ?? 'pre', $_GET['true'] ?? 'true', __FILE__, __LINE__);
+],
+
+array('羁', '绊', '过', '客'),
+
+$_GET['pre'] ?? null,
+
+$_GET['true'] ?? 'true',
+
+array(
+    __FILE__,
+    __LINE__,
+    'func' => array(
+        'extract' => array(
+            'var_array' => array(
+                'arg' => null,
+                'err' => 0,
+                'var' => 0,
+                'lnk' => null,//1
+            ),
+        ),
+    ),
+),
+
+['羁', '绊'],
+
+);
+*/
